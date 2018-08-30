03db5b029447bbcd5963a85ec6232ba63b70e02d
yicduan <victoriaduanyc@gmail.com>
2018-08-30 21:39:32 +0900

    initial commit

---
+++ Pigeon Simulator/Assets/Scripts/Flappy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flappy : MonoBehaviour {
+
+    private GameObject left;
+    private GameObject right;
+
+    private GameObject body;
+
+    private void Start() {
+
+        left = GameObject.Find("Left");
+        right = GameObject.Find("Right");
+        body = GameObject.Find("Body");
+    }
+
+    private void FixedUpdate() {
+
+    }
+}

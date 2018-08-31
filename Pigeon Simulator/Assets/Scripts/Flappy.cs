b0074c4f6f939f786e9ac5a44cdcd4d8adca6ff6
yicduan <victoriaduanyc@gmail.com>
2018-08-31 12:02:48 +0900

    naive movement

--- Pigeon Simulator/Assets/Scripts/Flappy.cs
+++ Pigeon Simulator/Assets/Scripts/Flappy.cs
@@ -4,19 +4,64 @@ using UnityEngine;
 
 public class Flappy : MonoBehaviour {
 
-    private GameObject left;
-    private GameObject right;
+    public GameObject left;
+    public GameObject right;
 
-    private GameObject body;
+    public GameObject body;
+
+    public Rigidbody bodyRb;
+    public Rigidbody leftRb;
+    public Rigidbody rightRb;
+
+    private float forwardSpeed = 0.1f;
+    private float upSpeed = 20;
+    private float horizontalSpeed = 20;
+
+    private Vector3 upMovement;
+    private Vector3 forwardMovement;
 
     private void Start() {
 
         left = GameObject.Find("Left");
         right = GameObject.Find("Right");
         body = GameObject.Find("Body");
+
+        bodyRb = body.GetComponent<Rigidbody>();
+        leftRb = left.GetComponent<Rigidbody>();
+        rightRb = right.GetComponent<Rigidbody>();
+
+        Physics.gravity = 0.05f * Vector3.down;
+
+        upMovement = upSpeed * Vector3.up;
+        forwardMovement = forwardSpeed * Vector3.right;
     }
 
-    private void FixedUpdate() {
+    private void Update() {
+
+        body.transform.Translate(forwardMovement * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            // Left
+            LiftLeft();
+
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            // Right
+            LiftRight();
+            
+    }
+
+    private void LiftLeft() {
+        body.transform.Translate(upMovement * Time.deltaTime);
+        body.transform.Rotate(Vector3.up, horizontalSpeed * Time.deltaTime);
+    }
+
+    private void LiftRight() {
+        body.transform.Translate(upMovement * Time.deltaTime);
+        body.transform.Rotate(Vector3.up, -horizontalSpeed * Time.deltaTime);
+    }
+
+    private void Apply(Vector3 movement) {
 
     }
+
 }

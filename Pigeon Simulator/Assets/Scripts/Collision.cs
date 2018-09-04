95cadf3b2d4d07ed2d1439195805037ca42e2cf0
Ruolan <fionaxrl@gmail.com>
2018-09-04 19:21:57 +0800

    add bird collision

---
+++ Pigeon Simulator/Assets/Scripts/Collision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+
+	void OnCollisionEnter(UnityEngine.Collision col)
+	{
+		if (col.gameObject.name != "Left" && col.gameObject.name != "Right")
+		{
+			Debug.Log("Collision detected!");
+			Debug.Log(col.gameObject.name);
+			//TODO: ADD DEATH UI
+
+		}
+
+	}
+
+}

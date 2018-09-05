c2f11e86366ccee2cabf25623c47f16e4af8cee1
Xie Jihui <xjh125470939@gmail.com>
2018-09-05 16:09:18 +0800

    no message

--- Pigeon Simulator/Assets/Scripts/GetScore.cs
+++ Pigeon Simulator/Assets/Scripts/GetScore.cs
@@ -2,12 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GetScore : MonoBehaviour {
 
     public Text score;
 
 	void Start () {
-        score.text = "Score: " + PlayerPrefs.GetInt("score");
+        if (SceneManager.GetActiveScene().name.Equals("Death"))
+        {
+            score.text = "Score: " + PlayerPrefs.GetInt("score");
+        } else {
+            score.text = "Score: 0";
+        }
+    }
+
+    void Update()
+    {
+        if (!SceneManager.GetActiveScene().name.Equals("Death"))
+        {
+            score.text = "Score: " + (int)(Time.timeSinceLevelLoad * 5);
+        }
     }
 }

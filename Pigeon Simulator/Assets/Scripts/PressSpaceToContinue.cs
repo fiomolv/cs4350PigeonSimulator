309be031226d5b74bca3d93d6104052a2239b83a
XieJihui <xjh125470939@gmail.com>
2018-09-07 21:02:36 +0800

    Change tutorial panel control

---
+++ Pigeon Simulator/Assets/Scripts/PressSpaceToContinue.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressSpaceToContinue : MonoBehaviour {
+
+    public GameObject panel;
+
+    void Update()
+    {
+        if (panel.activeSelf && Input.GetKeyDown("space"))
+        {
+            panel.SetActive(false);
+        }
+    }
+}

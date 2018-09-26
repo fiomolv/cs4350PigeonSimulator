46f8327ad610dcebd9c134989e27abd28eb4436d
XieJihui <xjh125470939@gmail.com>
2018-09-26 18:06:50 +0800

    Create a tree falling down with a specific distance of the bird

---
+++ Pigeon Simulator/Assets/Scripts/TreeFallDown.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeFallDown : MonoBehaviour {
+
+    public GameObject body;
+    public GameObject tree;
+    public AudioClip audio;
+    public int distance;
+    public int fallDownSpeed;
+
+    private bool startFallDown;
+    private bool playAudio;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        startFallDown = false;
+        playAudio = false;
+        audioSource = (AudioSource)tree.AddComponent<AudioSource>();
+        audioSource.clip = audio;
+        audioSource.loop = false;
+    }
+
+	void Update () {
+		if(!startFallDown && body.transform.position.x > tree.transform.position.x - distance)
+        {
+            startFallDown = true;
+        }
+
+        if (startFallDown && tree.transform.rotation.z < 0.4)
+        {
+            tree.transform.Rotate(-Vector3.right * Time.deltaTime * fallDownSpeed, Space.World);
+        }
+
+        if (!playAudio && startFallDown)
+        {
+            playAudio = true;
+            audioSource.Play(0);
+        }
+	}
+}

aa10093e196e07d18f64be1962023b740779b0ac
Xie Jihui <xjh125470939@gmail.com>
2018-09-27 17:24:02 +0800

    A prefab with rotating fan

---
+++ Pigeon Simulator/Assets/Scripts/FanRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanRotation : MonoBehaviour {
+
+    public GameObject fan;
+    public int speed;
+
+	// Update is called once per frame
+	void Update () {
+        fan.transform.Rotate(Vector3.right * Time.deltaTime * speed);
+	}
+}

88e4c0a2b4a2ec8788ecdb7155ad25b1413b73b6
Ruolan <fionaxrl@gmail.com>
2018-11-03 20:34:20 +0800

    add deadpage animation & fix map animation

---
+++ Pigeon Simulator/Assets/Scripts/LoopAnimation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopAnimation : MonoBehaviour {
+
+	public Animation anim;
+	public string name;
+
+	void Start()
+	{
+		// Set the wrap mode of the walk animation to loop
+		anim[name].wrapMode = WrapMode.Loop;
+	}
+}
